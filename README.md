b542b1a3338a814244da6573c6ee4284f2960f6f
enigmurl <manu.s.bhat@gmail.com>
2022-06-02 14:05:57 -0700

    improve navbar

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ SVG icons were sourced from the following locations:
 * https://icons8.com/icon/60444/linkedin-circled
 * https://icons8.com/icon/30888/discord 
 * https://icons8.com/icon/32309/instagram 
+    * https://www.logo.wine/logo/Instagram 
 * https://icons8.com/icon/118466/facebook
--- src/commons.js
+++ src/commons.js
@@ -24,21 +24,37 @@ function StandardTemplate(props) {
     );
 }
 
+function ExpandNavbar() {
+    console.log("clicked");
+    const navbar = document.getElementById("navbar")
+    if (!navbar.classList.contains("expanded")) {
+        navbar.classList.add("expanded");
+    } else {
+        navbar.classList.remove("expanded");
+    }
+}
+
 /* NAVIGATION */
 function Navbar(props) {
     return (
         <nav id='navbar'>
-            {/* actual Manu Bhat title */}
-            <a href='/'>Manu Bhat</a>
-            {/* separate buttons from title*/}
-            <ul id='nav-buttons'>  
-                {/* Listed in right first order */}
-                <Navitem title='Work | Large Projects' href='/work' active = {props.active}/>
-                <Navitem title='Extracurriculars' href='/extracurriculars' active = {props.active}/>
-                <Navitem title='Academics | Honors' href='/academics' active = {props.active}/>
-                <Navitem title='Skills' href='/skills' active = {props.active}/>
-                <Navitem title='Contact' href='/contact' active = {props.active}/>
-            </ul>
+            <span id='navbar-main'> 
+                {/* actual Manu Bhat title */}
+                <a href='/'>Manu Bhat</a>
+                {/* separate buttons from title*/}
+                <ul id='nav-buttons'>  
+                    {/* Listed in right first order */}
+                    <Navitem title='Work | Large Projects' href='/work' active = {props.active}/>
+                    <Navitem title='Extracurriculars' href='/extracurriculars' active = {props.active}/>
+                    <Navitem title='Academics | Honors' href='/academics' active = {props.active}/>
+                    <Navitem title='Skills' href='/skills' active = {props.active}/>
+                    <Navitem title='Contact' href='/contact' active = {props.active}/>
+                </ul>
+            </span>
+            <button id='navbar-expand-button' onClick={ExpandNavbar}>
+                {/* Belive this is from bootstrap, but not sure */}
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30"><path stroke="rgba(33,33,33,0.5)" strokeWidth="2" strokeLinecap="round" strokeMiterlimit="10" d="M4 8h24M4 16h24M4 24h24"/></svg>
+            </button>
         </nav>
     );
 }
